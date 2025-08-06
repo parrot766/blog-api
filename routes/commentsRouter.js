9fa051398643af4a6b8539762bb5066d0dd1a0c0
parrot <guilhermezx3@hotmail.com>
2025-08-06 17:54:33 -0300

    Add commetsRouter

---
+++ routes/commentsRouter.js
@@ -0,0 +1,17 @@
+const { Router } = require("express");
+const verifyToken = require("../middlewares/auth");
+const {
+  getComments,
+  postComment,
+  putComment,
+  deleteComment,
+} = require("../controllers/commentController");
+
+const commentsRouter = Router({ mergeParams: true });
+
+commentsRouter.get("/", verifyToken, getComments);
+commentsRouter.post("/", verifyToken, postComment);
+commentsRouter.put("/:commentUuid", verifyToken, putComment);
+commentsRouter.delete("/:commentUuid", verifyToken, deleteComment);
+
+module.exports = commentsRouter;
