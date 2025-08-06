8e6ba2f569a3fe79822f2899e407968b2fb6dd1e
parrot <guilhermezx3@hotmail.com>
2025-08-06 17:52:17 -0300

    Add postsController

---
+++ controllers/postsController.js
@@ -0,0 +1,93 @@
+const posts = require("../models/posts");
+const jwt = require("jsonwebtoken");
+
+async function getPosts(req, res) {
+  const allPosts = await posts.getAllPublishedPosts();
+
+  jwt.verify(req.token, process.env.JWT_SECRET, (err) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({ posts: allPosts });
+    }
+  });
+}
+
+async function createPost(req, res) {
+  const { title, content, published } = req.body;
+  const isPublished = published === "true";
+
+  jwt.verify(req.token, process.env.JWT_SECRET, (err, data) => {
+    if (err) {
+      res.status(401).json({ error: "Unauthorized" });
+    } else {
+      posts.createPost(title, content, isPublished, data.user.id);
+
+      res.json({ message: "post created successfully" });
+    }
+  });
+}
+
+async function getPostByUuid(req, res) {
+  const { postUuid } = req.params;
+
+  jwt.verify(req.token, process.env.JWT_SECRET, async (err) => {
+    if (err) {
+      res.status(401).json({ error: "Unauthorized" });
+    } else {
+      const post = await posts.getPostByUuid(postUuid);
+      if (!post) {
+        return res.status(404).json({ error: "Post not found" });
+      }
+
+      res.json({ post });
+    }
+  });
+}
+
+async function putPost(req, res) {
+  const { postUuid } = req.params;
+  const { title, content } = req.body;
+
+  jwt.verify(req.token, process.env.JWT_SECRET, async (err) => {
+    if (err) {
+      res.status(401).json({ error: "Unauthorized" });
+    } else {
+      const post = await posts.getPostByUuid(postUuid);
+      if (!post) {
+        return res.status(404).json({ error: "Not found" });
+      }
+
+      const updatedPost = await posts.updatePost(postUuid, title, content);
+
+      res.json({ post: updatedPost });
+    }
+  });
+}
+
+async function deletePost(req, res) {
+  const { postUuid } = req.params;
+
+  jwt.verify(req.token, process.env.JWT_SECRET, async (err) => {
+    if (err) {
+      res.status(401).json({ error: "Unauthorized" });
+    } else {
+      const post = await posts.getPostByUuid(postUuid);
+      if (!post) {
+        return res.status(404).json({ error: "Post not found" });
+      }
+
+      await posts.deletePost(postUuid);
+
+      res.json({ message: "post deleted successfully" });
+    }
+  });
+}
+
+module.exports = {
+  getPosts,
+  createPost,
+  getPostByUuid,
+  putPost,
+  deletePost,
+};
