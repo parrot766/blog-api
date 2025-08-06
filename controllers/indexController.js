f85c54db3279c166024e37af32fe0d5950e2b97a
parrot <guilhermezx3@hotmail.com>
2025-08-06 17:52:08 -0300

    Add indexController

---
+++ controllers/indexController.js
@@ -0,0 +1,45 @@
+const users = require("../models/users");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+function getIndex(req, res) {
+  res.json({ message: "hello" });
+}
+
+async function createUser(req, res) {
+  const { name, email, password } = req.body;
+
+  const userExists =
+    (await users.getUserByName(name)) || (await users.getUserByEmail(email));
+  if (userExists) {
+    return res.status(409).json({ error: "user already exists" });
+  }
+
+  await users.createUser(name, email, password);
+
+  res.json({ message: "user created successfully" });
+}
+
+async function postLogin(req, res) {
+  const { name, password } = req.body;
+
+  const user = await users.getUserByName(name);
+  if (!user) {
+    return res.status(401).json({ error: "invalid credentials" });
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    return res.status(401).json({ error: "invalid credentials" });
+  }
+
+  jwt.sign({ user }, process.env.JWT_SECRET, (err, token) => {
+    res.json({ token });
+  });
+}
+
+module.exports = {
+  getIndex,
+  createUser,
+  postLogin,
+};
