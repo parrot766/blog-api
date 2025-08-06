9e65a9980d6568dbfffd9b91bd7a0403d3d5cc3e
parrot <guilhermezx3@hotmail.com>
2025-08-06 17:53:45 -0300

    Add posts model

---
+++ models/posts.js
@@ -0,0 +1,88 @@
+const prisma = require("../prisma/client");
+
+async function getAllPublishedPosts() {
+  return await prisma.post.findMany({
+    where: { isPublished: true },
+    select: {
+      uuid: true,
+      title: true,
+      content: true,
+      createdAt: true,
+      updatedAt: true,
+      author: {
+        select: {
+          name: true,
+          email: true,
+        },
+      },
+    },
+  });
+}
+
+async function createPost(title, content, isPublished, authorId) {
+  return await prisma.post.create({
+    data: {
+      title,
+      content,
+      isPublished,
+      authorId,
+    },
+  });
+}
+
+async function getPostByUuid(postUuid) {
+  return await prisma.post.findUnique({
+    where: { uuid: postUuid },
+    select: {
+      id: true,
+      uuid: true,
+      title: true,
+      content: true,
+      createdAt: true,
+      updatedAt: true,
+      author: {
+        select: {
+          name: true,
+          email: true,
+        },
+      },
+    },
+  });
+}
+
+async function updatePost(postUuid, title, content) {
+  return await prisma.post.update({
+    where: { uuid: postUuid },
+    data: {
+      title,
+      content,
+    },
+    select: {
+      uuid: true,
+      title: true,
+      content: true,
+      createdAt: true,
+      updatedAt: true,
+      author: {
+        select: {
+          name: true,
+          email: true,
+        },
+      },
+    },
+  });
+}
+
+async function deletePost(postUuid) {
+  return await prisma.post.delete({
+    where: { uuid: postUuid },
+  });
+}
+
+module.exports = {
+  getAllPublishedPosts,
+  createPost,
+  getPostByUuid,
+  updatePost,
+  deletePost,
+};
