d1f24f6a88c4288ebc438c41cdf2b87ddc395a34
parrot <guilhermezx3@hotmail.com>
2025-08-13 15:12:58 -0300

    Update authRouter route to use '/auth' path

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", authRouter);
+app.use("/auth", authRouter);
 app.use("/posts", postsRouter);
 app.use("/posts/:postUuid/comments", commentsRouter);
 app.use("/user", usersRouter);
