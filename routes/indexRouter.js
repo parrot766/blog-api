23d970740d7cdf28cbc0ffa4ed7944a4949a6a0e
parrot <guilhermezx3@hotmail.com>
2025-08-13 14:58:57 -0300

    Remove getIndex route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,13 +1,8 @@
 const { Router } = require("express");
-const {
-  getIndex,
-  postSignup,
-  postLogin,
-} = require("../controllers/indexController");
+const { postSignup, postLogin } = require("../controllers/indexController");
 
 const indexRouter = Router();
 
-indexRouter.get("/", getIndex);
 indexRouter.post("/signup", postSignup);
 indexRouter.post("/login", postLogin);
 
