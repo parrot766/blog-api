b76449cf0cb655670c09e8e3782beef5771a056f
parrot <guilhermezx3@hotmail.com>
2025-09-01 14:52:41 -0300

    delete indexRouter.js

--- routes/indexRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { postSignup, postLogin } = require("../controllers/indexController");
-
-const indexRouter = Router();
-
-indexRouter.post("/signup", postSignup);
-indexRouter.post("/login", postLogin);
-
-module.exports = indexRouter;
